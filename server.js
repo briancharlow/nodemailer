7352dbe3313a259a7d0b6b4ff997ca49c402e4e5
briancharlow <mwangangi.brian20@students.dkut.ac.ke>
2023-06-23 11:30:08 +0300

    makes changes to the server file

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 const nodemailer = require('nodemailer');
-const cron = require('cron');
 const dotenv = require('dotenv');
 const ejs = require('ejs');
+const cron = require('node-cron');
 
 dotenv.config();
 const transporter = nodemailer.createTransport({
@@ -22,23 +22,11 @@ function generateWeeklyReport() {
         if (err) {
             console.error('Error generating the weekly report:', err);
         } else {
-            const emailBody = `<p>Dear Jonathan,</p>
-                        <p>Please find attached my weekly report.</p>
-                        <p>Kind regards,</p>
-                        <p>Brian Kyalo</p>`;
-
             const mailOptions = {
                 from: process.env.EMAIL_USER,
                 to: 'jonathan.mwaniki@thejitu.com',
                 subject: 'Weekly Report',
-                html: emailBody,
-                attachments: [
-                    {
-                        filename: 'weekly_report.docx',
-                        content: html,
-                        contentType: 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
-                    }
-                ]
+                html: html
             };
 
             transporter.sendMail(mailOptions, (error, info) => {
@@ -51,9 +39,10 @@ function generateWeeklyReport() {
         }
     });
 }
-
-
-const job = new cron.CronJob('0 14 * * 5', generateWeeklyReport, null, true, 'UTC');
+cron.schedule('0 14 * * 5', generateWeeklyReport, {
+    scheduled: true,
+    timezone: 'UTC'
+});
 
 app.listen(3600, () => {
     console.log('Server is running on port 3600');
